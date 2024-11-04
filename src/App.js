099ffb01cb2813d5f2b8301556faedb732da2833
elena7993 <elenalee7993@gmail.com>
2024-11-04 14:49:54 +0900

    04_11_24

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const App = () => {
+  const [num, setNum] = useState(0);
+  return (
+    <div>
+      <h3>{num}</h3>
+      <button onClick={() => setNum(num + 1)}>+</button>
+      <button onClick={() => setNum(num - 1)}>-</button>
+    </div>
+  );
+};
+
+export default App;
